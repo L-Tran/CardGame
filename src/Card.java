e4de15ed5c89062ee029d72e351a43db6f3f1fb3
L-Tran <logan.tran@menloschool.org>
2024-11-18 09:38:33 -0800

    Finished the Card Class

--- src/Card.java
+++ src/Card.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
 public class Card {
+    String rank;
+    String suit;
+    int value;
+
+    // Constructor
+    public Card(String suit, String rank, int value) {
+        this.suit = suit;
+        this.rank = rank;
+        this.value = value;
+    }
+
+    // Getter & setter methods
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getSuit() {
+        return suit;
+    }
+
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    // ToString
+    @Override
+    public String toString() {
+        return this.rank + " of " + this.suit;
+    }
 }
