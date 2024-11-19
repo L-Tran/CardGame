1066d2ca942c8f2136ad3bd24530ba942f7fb9af
L-Tran <logan.tran@menloschool.org>
2024-11-19 15:07:21 -0800

    Finished Deck Class

--- src/Deck.java
+++ src/Deck.java
@@ -31,4 +31,26 @@ public class Deck {
     public int getCardsLeft() {
         return cardsLeft;
     }
+
+    public Card deal() {
+        // If deck is empty
+        if (isEmpty()) {
+            return null;
+        }
+        // Decrease cards and return last card
+        cardsLeft--
+        return cards.get(cardsLeft);
+    }
+
+    // Shuffle cards
+    public void shuffle() {
+        for (int i = cardsLeft - 1; i > 0, i--) {
+            int r = (int)(Math.random() * (i + 1);
+            cards.add(r, cards.remove(i));
+            cards.remove(cards.remove(r + 1));
+        }
+
+    }
+
+
 }
