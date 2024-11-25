c5a893736bf3458c39da0602bad2ea9f03e97aa2
L-Tran <logan.tran@menloschool.org>
2024-11-24 23:16:04 -0800

    Working on Game Class

--- src/Game.java
+++ src/Game.java
@@ -34,31 +34,84 @@ public class Game {
             hand1.add(deck.deal());
             hand2.add(deck.deal());
         }
-        Player p1 = new Player(name1, hand1);
-        Player p2 = new Player(name2, hand2);
+        p1 = new Player(name1, hand1);
+        p2 = new Player(name2, hand2);
 
         // Game has not been one
         hasWon = false;
 
+        // Play Game
+        while (!hasWon) {
+            // Find winner of round
+            Player winner = compareCard(p1, p2);
+            Player loser;
+            if (winner == p1) {
+                loser = p2;
+            }
+            else {
+                loser = p1;
+            }
+            // Print the winner and cards
+            printWinner(winner, loser);
+            // Check if Game is won
+            hasWon = checkWin(p1, p2);
+        }
     }
 
+    // Print Instructions
     public void printInstructions () {
-        System.out.println("The Deal\n" +
-                "The deck is divided evenly, with each player receiving 26 cards, dealt one at a time, face down. " +
-                "P1 deals first. Each player has their stack of cards face down.\n" +
-                "\n" + "The Play\n" +
-                "Each player turns up a card at the same time and the player with the higher card takes both cards and" +
-                " puts them, face down, on the bottom of his stack.\n" +
-                "\n" +
-                "If the cards are the same rank, it is War. Each player turns up three cards face down. Then each" +
-                " player places a card face up. The player with the higher cards takes both piles (10 cards). " +
-                "\n" +
-                "The Score\n" +
-                "The game ends when one player has won all the cards.");
+        System.out.println("""
+                The Deal
+                The deck is divided evenly, with each player receiving 26 cards, dealt one at a time, face down. \
+                P1 deals first. Each player has their stack of cards face down.
+                
+                The Play
+                Each player turns up a card at the same time and the player with the higher card takes both cards and\
+                 puts them, face down, on the bottom of his stack.
+                
+                If the cards are the same rank, it is War. Each player turns up three cards face down. Then each\
+                 player places a card face up. The player with the higher cards takes both piles (10 cards). \
+                
+                The Score
+                The game ends when one player has won all the cards.
+                """);
     }
 
 
+    // Compare cards
+    public Player compareCard(Player p1, Player p2) {
+        if (p1.getHand().get(0).getValue() > p2.getHand().get(0).getValue()) {
+            return p1;
+        }
+        return p2;
+    }
 
+    // Print winner of round and adjust cards
+    public void printWinner(Player winner, Player loser) {
+        System.out.println(winner.getName() +  " won" +
+                " the round because they had a " + winner.getHand().get(0).getRank() + " and " + loser.getName() +
+                " had a " + loser.getHand().get(0).getRank());
+        winner.getHand().add(winner.getHand().size() - 1, winner.getHand().remove(0));
+        winner.getHand().add(winner.getHand().size() - 1, loser.getHand().remove(0));
+    }
+
+    // War
+
+    // Check for Win
+    public boolean checkWin(Player p1, Player p2) {
+        if (p1.getHand().isEmpty()|| p2.getHand().isEmpty()) {
+            if (p1.getHand().size() > p2.getHand().size()) {
+                    System.out.println("\n" + p1.getName() + " wins with all the cards");
+                    return true;
+                }
+                System.out.println("\n" + p2.getName() + " wins with all the cards");
+                return true;
+            }
+        return false;
+    }
+
+
+    // Main
     public static void main(String[] args) {
         // Start the game
         Game game = new Game();
